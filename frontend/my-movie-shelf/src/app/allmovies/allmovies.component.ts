65c7383d6ba0c594ba6c687c331ae31d1180f783
maccaabdi1 <143471838+maccaabdi1@users.noreply.github.com>
2025-06-18 16:30:17 -0400

    Time to work on this

--- frontend/my-movie-shelf/src/app/allmovies/allmovies.component.ts
+++ frontend/my-movie-shelf/src/app/allmovies/allmovies.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+interface Movie {
+  title: string;
+  status: string;
+  rating: number;
+  releaseDate: string;
+  watchedDate: string;
+  notes: string
+
+}
 
 @Component({
   selector: 'app-allmovies',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './allmovies.component.html',
   styleUrl: './allmovies.component.css'
 })
 export class AllMoviesComponent {
 
+  movies: Movie[] = [];
+
+  addMovie(){
+    this.movies.push({
+      title: '',
+      status: '',
+      rating: 0,
+      releaseDate: '',
+      watchedDate: '',
+      notes: ''
+    });
 }
+
+}
+
